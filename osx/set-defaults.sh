375c417669097726a16298a8191734cd93acb460
Lewis Juggins <ldjuggins@gmail.com>
2016-10-08 10:21:06 +0100

    Remove SmoothMouse, disable mouse acceleration

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -8,7 +8,10 @@ then
     #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
     #
     # Run ./set-defaults.sh and you'll be good to go.
-    
+
+    # Disable mouse acceleration.
+    defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
     # Disable press-and-hold for keys in favor of key repeat.
     defaults write -g ApplePressAndHoldEnabled -bool false
 
