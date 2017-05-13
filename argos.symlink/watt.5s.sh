6baa5ef4e2c8d31dcab2bbabb43636fb922df159
Lewis Juggins <ldjuggins@gmail.com>
2017-05-13 17:49:31 +0100

    Include second battery

--- argos.symlink/watt.5s.sh
+++ argos.symlink/watt.5s.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-WATT=$(awk '{print $1*10^-6 " W"}' /sys/class/power_supply/BAT1/power_now)
+BAT0_WATT=$(cat /sys/class/power_supply/BAT0/power_now)
+BAT1_WATT=$(cat /sys/class/power_supply/BAT1/power_now)
+
+SUM_WATT=$(($BAT0_WATT + $BAT1_WATT))
+
+WATT=$(awk -v sum_watt=$SUM_WATT ' BEGIN {print sum_watt*10^-6 " W"}' )
 
 echo "$WATT"
