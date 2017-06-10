1df1083a923abb734b22dbb6c00dc58ec9cee4c0
Lewis Juggins <ldjuggins@gmail.com>
2017-06-10 21:44:45 +0100

    Updates

--- argos.symlink/watt.5s.sh
+++ argos.symlink/watt.5s.sh
@@ -5,6 +5,8 @@ BAT1_WATT=$(cat /sys/class/power_supply/BAT1/power_now)
 
 SUM_WATT=$(($BAT0_WATT + $BAT1_WATT))
 
-WATT=$(awk -v sum_watt=$SUM_WATT ' BEGIN {print sum_watt*10^-6 " W"}' )
+WATT=$(awk -v sum_watt=$SUM_WATT ' BEGIN {print sum_watt*10^-6}' )
 
-echo "$WATT"
+FORMATTED=$(printf "%.1f" "$WATT")
+
+echo "$FORMATTED"W
