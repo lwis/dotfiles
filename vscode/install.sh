897c2392538c9f75ee57967f13d5c31306f46223
Lewis Juggins <ldjuggins@gmail.com>
2018-07-03 13:53:07 +0100

    Update vscode settings

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
 if test "$(which code)"; then
-  if [ "$(uname -s)" = "Darwin" ]; then
-    VSCODE_HOME="$HOME/Library/Application Support/Code"
-  else
-    VSCODE_HOME="$HOME/.config/Code"
-  fi
+    if [ "$(uname -s)" = "Darwin" ]; then
+        VSCODE_HOME="$HOME/Library/Application Support/Code"
+    else
+        VSCODE_HOME="$HOME/.config/Code"
+    fi
 
-  modules="
-    aaron-bond.better-comments
-    bungcip.better-toml
-    carolynvs.dep
-    eg2.tslint
-    HookyQR.beautify
-    ms-python.python
-    ms-vscode.cpptools
-    ms-vscode.Go
-    PeterJausovec.vscode-docker
-    PKief.material-icon-theme
-    redhat.java
-    rokoroku.vscode-theme-darcula
-    sidneys1.gitconfig
-  "
-  for module in $modules; do
-    code --install-extension "$module" || true
-  done
+    modules="
+        aaron-bond.better-comments
+        bungcip.better-toml
+        carolynvs.dep
+        donjayamanne.githistory
+        eamodio.gitlens
+        EditorConfig.EditorConfig
+        eg2.tslint
+        formulahendry.code-runner
+        HookyQR.beautify
+        ms-python.python
+        ms-vscode.cpptools
+        ms-vscode.Go
+        PeterJausovec.vscode-docker
+        PKief.material-icon-theme
+        redhat.java
+        rokoroku.vscode-theme-darcula
+        sidneys1.gitconfig
+    "
+    for module in $modules; do
+        code --install-extension "$module" || true
+    done
 fi
