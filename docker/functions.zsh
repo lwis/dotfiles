0a555be1ac8440d9af552979210bceddf483c94e
Lewis Juggins <github@lwis.me>
2020-10-13 07:45:31 +0100

    Updates (#1)

--- docker/functions.zsh
+++
@@ -1,7 +0,0 @@
-docker-stats() {
-    docker stats $(docker inspect -f "{{ .Name }}" $(docker ps -q))
-}
-
-docker-log() {
-    docker-compose logs -f
-}
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reload!='. ~/.zshrc'
+alias docker-cleanup='docker system prune --volumes --all'
